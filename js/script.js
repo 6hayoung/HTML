15995f4154f5a914032f207a44462c04f99dbc1b
6hayoung <6yue@kakao.com>
2023-03-02 16:05:00 +0900

    script

--- js/script.js
+++ js/script.js
@@ -1,20 +1,24 @@
 $(function(){
     //console.log('connect')
 
-    
-    
-    function newsInt(){
-        const $news = $('.news_bd > ul > li');
-        let newsIdx = $news.index();
-
-        $news.stop().animate({top:-18*newsIdx})
-        
-        
-        console.log(newsIdx)
-
-    }
-    $(window).on('load',function(){
-        newsInt()
+    //상단메뉴 fixed 이벤트
+    $(window).on('scroll',function(){
+        let topScroll = $(window).scrollTop();
+        //console.log(topScroll)
+        if(topScroll > 199) {
+            $('.fixed_top').removeClass('none')
+        }else{
+            $('.fixed_top').addClass('none')
+        }
     })
 
+   
+    //뉴스라인 자동 재생 이벤트
+    $('.news_bd > ul').slick({
+        autoplay:true,
+        arrows:false,
+        vertical:true,
+    })
+
+    //console.log($newsIndex)
 })
